07dab0bce1f369e860659604c054a00335fa9ef6
austinchabaud <austinc1023@yahoo.com>
2023-09-06 20:51:26 -0500

    auth is completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import { Icons } from './Icons';
 import { buttonVariants } from './ui/Button';
+import { getAuthSession } from '@/lib/auth';
 //dont forget to add async back
-const Navbar = () => {
+const Navbar = async () => {
+	const session = await getAuthSession();
 	return (
 		<div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
 			<div className='container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl'>
@@ -16,9 +18,13 @@ const Navbar = () => {
 
 				{/* search bar  */}
 
-				<Link href='/sign-in' className={buttonVariants()}>
-					Sign In
-				</Link>
+				{session ? (
+					<p> youre logged in</p>
+				) : (
+					<Link href='/sign-in' className={buttonVariants()}>
+						Sign In
+					</Link>
+				)}
 			</div>
 		</div>
 	);
