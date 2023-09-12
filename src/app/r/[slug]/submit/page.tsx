d764a5a8a0f527ff070f18b3c58aa2ef260d2ff4
austinchabaud <austinc1023@yahoo.com>
2023-09-12 17:15:05 -0500

    creating posts

---
+++ src/app/r/[slug]/submit/page.tsx
@@ -0,0 +1,21 @@
+import { db } from '@/lib/db';
+import { notFound } from 'next/navigation';
+
+interface PageProps {
+	params: {
+		slug: string;
+	};
+}
+
+const page = async ({ params }: PageProps) => {
+	const subreddit = await db.subreddit.findFirst({
+		where: {
+			name: params.slug,
+		},
+	});
+
+	if (!subreddit) return notFound();
+	return <div>page</div>;
+};
+
+export default page;
