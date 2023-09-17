1207131faf21551e87c829e485c4fc260ef1dde6
austinchabaud <austinc1023@yahoo.com>
2023-09-16 20:35:02 -0500

    able to upload image to a post, still working on upload

---
+++ src/components/Editor.tsx
@@ -0,0 +1,121 @@
+'use client';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import TextareaAutoSize from 'react-textarea-autosize';
+import { useForm } from 'react-hook-form';
+import { PostCreationRequest, PostValidator } from '@/lib/validators/post';
+import { zodResolver } from '@hookform/resolvers/zod';
+import EditorJS from '@editorjs/editorjs';
+import { uploadFiles } from '@/lib/uploadthing';
+
+interface EditorProps {
+	subredditId: string;
+}
+
+const Editor: FC<EditorProps> = ({ subredditId }) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<PostCreationRequest>({
+		resolver: zodResolver(PostValidator),
+		defaultValues: {
+			subredditId,
+			title: '',
+			content: null,
+		},
+	});
+
+	const ref = useRef<EditorJS>();
+	const [isMounted, setIsMounted] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			setIsMounted(true);
+		}
+	}, []);
+
+	const initializeEditor = useCallback(async () => {
+		const EditorJS = (await import('@editorjs/editorjs')).default;
+		const Header = (await import('@editorjs/header')).default;
+		const Embed = (await import('@editorjs/embed')).default;
+		const Table = (await import('@editorjs/table')).default;
+		const List = (await import('@editorjs/list')).default;
+		const Code = (await import('@editorjs/code')).default;
+		const LinkTool = (await import('@editorjs/link')).default;
+		const InlineCode = (await import('@editorjs/inline-code')).default;
+		const ImageTool = (await import('@editorjs/image')).default;
+
+		if (!ref.current) {
+			const editor = new EditorJS({
+				holder: 'editor',
+				onReady() {
+					ref.current = editor;
+				},
+				placeholder: 'Type here to write your post',
+				inlineToolbar: true,
+				data: { blocks: [] },
+				tools: {
+					header: Header,
+					linkTool: {
+						class: LinkTool,
+						config: {
+							endpoint: '/api/link',
+						},
+					},
+					image: {
+						class: ImageTool,
+						config: {
+							uploader: {
+								async uploadByFile(file: File) {
+									const [res] = await uploadFiles([file], 'imageUploader');
+
+									return {
+										success: 1,
+										file: {
+											url: res.fileUrl,
+										},
+									};
+								},
+							},
+						},
+					},
+					list: List,
+					code: Code,
+					inlineCode: InlineCode,
+					table: Table,
+					embed: Embed,
+				},
+			});
+		}
+	}, []);
+
+	useEffect(() => {
+		const init = async () => {
+			await initializeEditor();
+
+			setTimeout(() => {});
+		};
+
+		if (isMounted) {
+			init();
+
+			return () => {};
+		}
+	}, [isMounted, initializeEditor]);
+	return (
+		<div className='w-full p-4 border rounded-lg bg-zinc-50 border-zinc-200'>
+			<form id='subreddit-post-form' className='w-fit' onSubmit={() => {}}>
+				<div className='prose prose-stone dark:prose-invert'>
+					<TextareaAutoSize
+						placeholder='Title'
+						className='w-full overflow-hidden text-5xl font-bold bg-transparent appearance-none resize-none focus:outline-none'
+					/>
+
+					<div id='editor' className='min-h-[500px]' />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Editor;
